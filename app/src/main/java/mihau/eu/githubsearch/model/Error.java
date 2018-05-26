39e2fa75ab522c7580fb145795e52d1c45436351
kolorszczak <rachuba.michal@gmail.com>
2018-05-26 19:18:16 +0200

    Moved ErrorUtils to AppUtils

--- app/src/main/java/mihau/eu/githubsearch/model/Error.java
+++ app/src/main/java/mihau/eu/githubsearch/model/Error.java
@@ -1,11 +1,15 @@
 package mihau.eu.githubsearch.model;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.StringRes;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Error {
+import mihau.eu.githubsearch.utils.manager.ParcelableUtils;
+
+public class Error implements Parcelable {
 
     @SerializedName("message")
     public String message;
@@ -17,4 +21,42 @@ public class Error {
     public Error(String message) {
         this.message = message;
     }
+
+    public Error(Parcel in) {
+        this.message = ParcelableUtils.readStringHelper(in);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (getClass() != o.getClass()) return false;
+
+        Error error = (Error) o;
+        return this.message.equals(error.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return  2 * message.hashCode();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        ParcelableUtils.writeStringHelper(out, this.message);
+    }
+
+    public static final Creator<Error> CREATOR = new Creator<Error>() {
+
+        public Error createFromParcel(Parcel in) {
+            return new Error(in);
+        }
+
+        public Error[] newArray(int size) {
+            return new Error[size];
+        }
+    };
 }
--- app/src/main/java/mihau/eu/githubsearch/utils/manager/AppUtils.java
+++ app/src/main/java/mihau/eu/githubsearch/utils/manager/AppUtils.java
@@ -1,4 +1,42 @@
 package mihau.eu.githubsearch.utils.manager;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import mihau.eu.githubsearch.R;
+import mihau.eu.githubsearch.model.Error;
+import okhttp3.ResponseBody;
+import retrofit2.HttpException;
+
 public class AppUtils {
+
+    /**
+     * Method that is parsing throwable object into String error message
+     * @param context Context object
+     * @param throwable Throwable object
+     * @return String message parsed from throwable
+     */
+    public static String parseThrowable(Context context, Throwable throwable) {
+        Error error = new Error(context, R.string.serverError);
+        if (throwable instanceof HttpException) {
+            if (((HttpException) throwable).response().code() < 500) {
+                ResponseBody responseBody = ((HttpException) throwable).response().errorBody();
+                if (responseBody != null) {
+                    try {
+                        String json = responseBody.string().trim().replaceAll("\n", "").replaceAll("\r", "");
+                        error = new Gson().fromJson(json, Error.class);
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        } else if (throwable instanceof UnknownHostException || throwable instanceof SocketTimeoutException) {
+            error = new Error(context, R.string.noInternetConnection);
+        }
+        return error.message;
+    }
 }
