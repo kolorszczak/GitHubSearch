319e1807bfdcccdce11d42c4573b49e34b3feca8
kolorszczak <rachuba.michal@gmail.com>
2018-05-26 19:36:32 +0200

    Stetho initialize

--- app/src/main/java/mihau/eu/githubsearch/GitHubApplication.java
+++ app/src/main/java/mihau/eu/githubsearch/GitHubApplication.java
@@ -3,7 +3,15 @@ package mihau.eu.githubsearch;
 import android.app.Application;
 import android.content.Context;
 
+import com.facebook.stetho.Stetho;
+
 public class GitHubApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
 
     public static GitHubApplication getInstance(Context context) {
         return (GitHubApplication) context.getApplicationContext();
